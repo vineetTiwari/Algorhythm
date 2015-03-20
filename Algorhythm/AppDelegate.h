02745cd779737222a6588a1edd3a57842dbe5112
Vinny Tiwari <tiwari.vineet@outlook.com>
2015-03-19 20:54:33 -0700

    Initial Commit

---
+++ Algorhythm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorhythm
+//
+//  Created by Vineet Tiwari on 2015-03-19.
+//  Copyright (c) 2015 vinny.co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
